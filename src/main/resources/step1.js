0da797b1e14217aeeadc42d8efd15fab71f45efa
adamw <adam@warski.org>
2015-04-09 11:16:25 +0200

    JSONs

--- src/main/resources/step1.js
+++ src/main/resources/step1.js
@@ -5,11 +5,13 @@ var form = new Supler.Form(formContainer, {
 
 $(document).ready(function() {
     $.get('/rest/form1.json', function(data) {
+        $('#json-server').html(JSON.stringify(data));
         form.render(data);
     });
 });
 
 function sendForm(formValue, renderResponseFn, sendErrorFn) {
+    $('#json-client').html(JSON.stringify(formValue));
     $.ajax({
         url: '/rest/form1.json',
         type: 'POST',
